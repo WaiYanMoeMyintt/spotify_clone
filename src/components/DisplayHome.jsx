8c34aa096811a9c14d2dc85a6cb9c8f5ab112d10
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-05 15:07:15 +0630

    Add albums section

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import AlbumItems from "./AlbumItems";
+import { albumsData } from "../assets/assets";
 const DisplayHome = () => {
   return (
     <>
-       <Navbar />
+      <Navbar />
+      <div className="mb-4 font-bold text-2xl">
+        <h1 className="my-5 font-bold text-2xl">Features Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData &&
+            albumsData.map((items, index) => (
+              <AlbumItems
+                key={index}
+                name={items?.name}
+                desc={items?.desc}
+                id={items?.id}
+                image={items?.image}
+              />
+            ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
