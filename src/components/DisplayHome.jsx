3eb26aad85d04e4a2f785f14c48bd1090f3a4e52
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-05 15:15:25 +0630

    update biggest hits albumn

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 import AlbumItems from "./AlbumItems";
 import { albumsData } from "../assets/assets";
+import SongItems from "./SongItems";
+import { songsData } from "../assets/assets";
 const DisplayHome = () => {
   return (
     <>
@@ -21,6 +23,22 @@ const DisplayHome = () => {
             ))}
         </div>
       </div>
+      <div className="mb-4 font-bold text-2xl">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData &&
+            songsData.map((items, index) => (
+              <SongItems
+                key={index}
+                name={items?.name}
+                desc={items?.desc}
+                id={items?.id}
+                image={items?.image}
+              />
+            ))}
+        </div>
+      </div>
+      
     </>
   );
 };
